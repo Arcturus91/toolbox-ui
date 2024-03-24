c19f44d8931c1eb5cead35fa885b82c1ca2386a1
Arturo Barrantes Vasquez <arturo.barrantesv@pucp.pe>
2024-03-24 12:36:00 -0500

    Implement axios req to backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/files/data")
+      .then((axiosRes) => {
+        console.log("axios res:", axiosRes);
+      })
+      .catch((error) => {
+        console.log("LOGGING ERROR", error);
+      });
+
+    console.log("reloading");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi!</h1>
     </div>
   );
 }
